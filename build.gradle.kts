421a99dc135ff5b52b71a695e7c8ef25c2f6a16a
Kacper Świderski <kacpers2001@o2.pl>
2024-12-28 15:17:42 +0100

    Dodanie loggera i datastax do gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // SLF4J API
+    implementation("org.slf4j:slf4j-api:1.7.36")
+
+    // SLF4J Logger implementation (np. Logback)
+    implementation("ch.qos.logback:logback-classic:1.2.10")
+
+    // DataStax Cassandra Java Driver
+    implementation("com.datastax.cassandra:cassandra-driver-core:3.11.2")
 }
 
 tasks.test {

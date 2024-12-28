f5f3f0abf5389a01819dca3dc3cc20a6ebe73012
Kacper Świderski <kacpers2001@o2.pl>
2024-12-28 16:45:51 +0100

    dodawanie i usuwanie zamówień

--- src/main/java/com/trains/Main.java
+++ src/main/java/com/trains/Main.java
@@ -1,7 +1,9 @@
 package com.trains;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.Properties;
+import java.util.UUID;
 
 import com.trains.backend.BackendException;
 import com.trains.backend.BackendSession;
@@ -26,8 +28,28 @@ public class Main {
 			
 		BackendSession session = new BackendSession(contactPoint, keyspace);
 
+
+		session.upsertOrder(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"), 101, Timestamp.valueOf("2024-12-28 08:00:00"),
+				UUID.fromString("850e8400-e29b-41d4-a716-446655440001"), 2, 4);
+
+		session.upsertOrder(UUID.fromString("550e8400-e29b-41d4-a716-446655440002"), 102, Timestamp.valueOf("2024-12-28 09:30:00"),
+				UUID.fromString("850e8400-e29b-41d4-a716-446655440003"), 1, 3);
+
+		session.upsertOrder(UUID.fromString("550e8400-e29b-41d4-a716-446655440004"), 103, Timestamp.valueOf("2024-12-28 11:00:00"),
+				UUID.fromString("850e8400-e29b-41d4-a716-446655440005"), 4, 2);
+
+		session.upsertOrder(UUID.fromString("550e8400-e29b-41d4-a716-446655440006"), 104, Timestamp.valueOf("2024-12-28 12:30:00"),
+				UUID.fromString("850e8400-e29b-41d4-a716-446655440007"), 5, 6);
+
+		session.upsertOrder(UUID.fromString("550e8400-e29b-41d4-a716-446655440008"), 105, Timestamp.valueOf("2024-12-28 15:00:00"),
+				UUID.fromString("850e8400-e29b-41d4-a716-446655440009"), 6, 8);
+
+
+
 		String output = session.selectAllOrders();
 		System.out.println("Orders: \n" + output);
+
+		session.deleteAllOrders();
 
 		System.exit(0);
 	}
--- src/main/java/com/trains/backend/BackendSession.java
+++ src/main/java/com/trains/backend/BackendSession.java
@@ -10,6 +10,7 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.UUID;
 
@@ -45,6 +46,8 @@ public class BackendSession {
 	private static PreparedStatement INSERT_INTO_USERS;
 	private static PreparedStatement DELETE_ALL_FROM_USERS;
 	private static PreparedStatement SELECT_ALL_FROM_ORDERS;
+	private static PreparedStatement INSERT_INTO_ORDERS;
+	private static PreparedStatement DELETE_ALL_FROM_ORDERS;
 
 
 	private static final String USER_FORMAT = "- %-10s  %-16s %-10s %-10s\n";
@@ -61,8 +64,8 @@ public class BackendSession {
 
 			// Statements for the 'orders' table
 			SELECT_ALL_FROM_ORDERS = session.prepare("SELECT * FROM orders;");
-			//INSERT_INTO_ORDERS = session.prepare("INSERT INTO orders (order_id, train_id, trip_date, user_id, car, seats_amount) VALUES (?, ?, ?, ?, ?, ?);");
-			//DELETE_ALL_FROM_ORDERS = session.prepare("TRUNCATE orders;");
+			INSERT_INTO_ORDERS = session.prepare("INSERT INTO orders (order_id, train_id, trip_date, user_id, car, seats_amount) VALUES (?, ?, ?, ?, ?, ?);");
+			DELETE_ALL_FROM_ORDERS = session.prepare("TRUNCATE orders;");
 
 			// Statements for the 'trains' table
 			//SELECT_ALL_FROM_TRAINS = session.prepare("SELECT * FROM trains;");
@@ -115,6 +118,29 @@ public class BackendSession {
 		return builder.toString();
 	}
 
+	public void upsertOrder(UUID orderId, int trainId, Timestamp tripDate, UUID userId, int car, int seatsAmount) throws BackendException {
+		BoundStatement bs = new BoundStatement(INSERT_INTO_ORDERS);
+		bs.bind(orderId, trainId, tripDate, userId, car, seatsAmount);
 
+		try {
+			session.execute(bs);
+		} catch (Exception e) {
+			throw new BackendException("Could not perform an upsert. " + e.getMessage() + ".", e);
+		}
+
+		logger.info("Order " + orderId + " upserted");
+	}
+
+	public void deleteAllOrders() throws BackendException {
+		BoundStatement bs = new BoundStatement(DELETE_ALL_FROM_ORDERS);
+
+		try {
+			session.execute(bs);
+		} catch (Exception e) {
+			throw new BackendException("Could not perform a delete operation. " + e.getMessage() + ".", e);
+		}
+
+		logger.info("All orders deleted");
+	}
 
 }

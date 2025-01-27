c3aea0ddb1a2b68b91238ad95a6066bdd1bd356d
Karolina Sztolcman <87234867+kklalolina@users.noreply.github.com>
2025-01-27 20:46:07 +0100

    add tests

--- src/test/java/com/trains/OrderServiceTestManyUsers.java
+++ src/test/java/com/trains/OrderServiceTestManyUsers.java
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public class OrderServiceTestQUORUM {
+public class OrderServiceTestManyUsers {
 
     private static BackendSession session;
     private static OrderService orderService;
@@ -31,9 +34,20 @@ public class OrderServiceTestQUORUM {
         reservationService = session.getReservationService();
 
         // Setup initial data
-        UUID userId = UUID.randomUUID();
-        userService.upsertUser(userId, "Test User");
-        users.add(userId);
+        UUID user1 = UUID.fromString("11111111-1111-1111-1111-111111111111");
+        UUID user2 = UUID.fromString("22222222-2222-2222-2222-222222222222");
+        UUID user3 = UUID.fromString("33333333-3333-3333-3333-333333333333");
+        UUID user4 = UUID.fromString("44444444-4444-4444-4444-444444444444");
+        if (userService.getUser(user1) == null) {
+            userService.upsertUser(user1, "Jon Snow");
+            userService.upsertUser(user2, "Jane Doe");
+            userService.upsertUser(user3, "Alice Cooper");
+            userService.upsertUser(user4, "Bob Marley");
+            users.add(user1);
+            users.add(user2);
+            users.add(user3);
+            users.add(user4);
+        }
         trains.add(trainService.upsertTrain(4091, Timestamp.valueOf("2024-12-28 11:00:00"), 5, 50));
         trains.add(trainService.upsertTrain(4092, Timestamp.valueOf("2024-12-28 12:00:00"), 4, 60));
         trains.add(trainService.upsertTrain(4093, Timestamp.valueOf("2024-12-28 13:00:00"), 6, 40));
@@ -41,7 +55,7 @@ public class OrderServiceTestQUORUM {
 
     @Test
     public void benchmarkInsertOrdersWithMultipleThreads() {
-        int numberOfThreads = 1000;
+        int numberOfThreads = 100;
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
 
         long startTime = System.currentTimeMillis();
@@ -50,7 +64,7 @@ public class OrderServiceTestQUORUM {
             executorService.submit(() -> {
                 assertDoesNotThrow(() -> {
                     String train = trains.get(new Random().nextInt(trains.size()));
-                    addTicket(train, users.get(0), 1);
+                    addTicket(train, users.get(new Random().nextInt(users.size())), new Random().nextInt(10)+1 );
                 });
             });
         }
@@ -67,18 +81,7 @@ public class OrderServiceTestQUORUM {
 
         System.out.println("Benchmark completed in: " + duration + " ms");
 
-        // Add the select statement here
-        String query = "SELECT train_id, trip_date, car, SUM(seats_amount) FROM orders GROUP BY train_id, trip_date, car";
-        final int[] previousTrainId = {-1};
-        session.getSession().execute(query).forEach(row -> {
-            int currentTrainId = row.getInt("train_id");
-            if (currentTrainId != previousTrainId[0] && previousTrainId[0] != -1) {
-                System.out.println();
-            }
-            System.out.println(String.format("Train ID: %d, Trip Date: %s, Car: %d, Seats Amount: %d",
-                    currentTrainId, row.getTimestamp("trip_date"), row.getInt("car"), row.getInt("system.sum(seats_amount)")));
-            previousTrainId[0] = currentTrainId;
-        });
+
     }
 
     private void addTicket(String train, UUID userId, int numberOfTickets) throws BackendException {
@@ -118,7 +121,6 @@ public class OrderServiceTestQUORUM {
         while (remainingTickets > 0) {
             reservedSeats = orderService.getTakenSeats(trainId, departureTime) + reservationService.getReservedSeats(trainId, departureTime);
             availableSeats = totalCapacity - reservedSeats;
-            System.out.println(availableSeats);
             if (availableSeats < numberOfTickets) {
                 System.out.println("Not enough seats available for the requested number of tickets.");
                 for (int car = 1; car <= cars; car++) { 
@@ -130,7 +132,6 @@ public class OrderServiceTestQUORUM {
             }
             int car = random.nextInt(cars) + 1;
             availableSeats = carCapacity - orderService.getTakenSeatsByCar(trainId, departureTime, car) - reservationService.getSumReservedSeatsByCar(trainId, departureTime, car);
-            System.out.println(availableSeats);
             if (availableSeats > 0) {
                 int ticketsToReserve = Math.min(remainingTickets, availableSeats);
                 int res = reservationService.reserveSeats(resId, trainId, Timestamp.valueOf(departureTime), userId, car, ticketsToReserve, carCapacity);
@@ -142,7 +143,6 @@ public class OrderServiceTestQUORUM {
                 remainingTickets -= ticketsToReserve;
                 System.out.println(ticketInfo);
                 reservationsSeats[car - 1] = ticketsToReserve;
-                //orderService.resolveConflictsForAllCars(trainId, Timestamp.valueOf(departureTime)); // Ensure conflicts are resolved after confirmation
             }
         }
 
@@ -155,7 +155,6 @@ public class OrderServiceTestQUORUM {
                     if (res == 0) {
                         System.out.println("Reservation failed. Please try again.");
                     }
-                    //orderService.resolveConflictsForAllCars(trainId, Timestamp.valueOf(departureTime)); // Ensure conflicts are resolved after confirmation
                 }
             }
         }

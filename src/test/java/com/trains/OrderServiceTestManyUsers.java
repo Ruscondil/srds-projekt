c0f69ef5e5a67002f6ed8d279ba355d409d6d965
Kacper Świderski <kacpers2001@o2.pl>
2025-01-28 13:02:34 +0100

    Wypisywanie na końcu testów

--- src/test/java/com/trains/OrderServiceTestManyUsers.java
+++ src/test/java/com/trains/OrderServiceTestManyUsers.java
@@ -81,7 +81,18 @@ public class OrderServiceTestManyUsers {
 
         System.out.println("Benchmark completed in: " + duration + " ms");
 
-
+        // Add the select statement here
+        String query = "SELECT train_id, trip_date, car, SUM(seats_amount) FROM orders GROUP BY train_id, trip_date, car";
+        final int[] previousTrainId = {-1};
+        session.getSession().execute(query).forEach(row -> {
+            int currentTrainId = row.getInt("train_id");
+            if (currentTrainId != previousTrainId[0] && previousTrainId[0] != -1) {
+                System.out.println();
+            }
+            System.out.println(String.format("Train ID: %d, Trip Date: %s, Car: %d, Seats Amount: %d",
+                    currentTrainId, row.getTimestamp("trip_date"), row.getInt("car"), row.getInt("system.sum(seats_amount)")));
+            previousTrainId[0] = currentTrainId;
+        });
     }
 
     private void addTicket(String train, UUID userId, int numberOfTickets) throws BackendException {
